ce7904e3b8709bc053c275a1d63accd72d100815
Juha Komulainen <juha.komulainen@evident.fi>
2016-04-11 16:48:14 +0300

    Fix equality for Type.Function

--- src/main/kotlin/siilinkari/types/Type.kt
+++ src/main/kotlin/siilinkari/types/Type.kt
@@ -1,5 +1,7 @@
 package siilinkari.types
 
+import java.util.Objects.hash
+
 /**
  * Represents the types supported in the language.
  */
@@ -14,5 +16,7 @@ sealed class Type {
 
     class Function(val argumentTypes: List<Type>, val returnType: Type) : Type() {
         override fun toString() = argumentTypes.joinToString(", ", "(", ")") + " -> " + returnType
+        override fun equals(other: Any?) = other is Function && argumentTypes == other.argumentTypes && returnType == other.returnType
+        override fun hashCode() = hash(argumentTypes, returnType)
     }
 }
